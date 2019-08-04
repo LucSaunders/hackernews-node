da051bd364afa52f1a03bdae9c4afc39431e4163
LucSaunders <lucsaunders@gmail.com>
2019-08-04 11:06:19 -0400

    Add feed with Link.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,38 @@ const { GraphQLServer } = require('graphql-yoga');
 
 // Define GraphQL schema
 const typeDefs = `type Query {
-    info: String!
-}`;
+  info: String!
+    feed: [Link!]!
+    
+},
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
+}
+`;
+
+// Add dummy data, store in links variate at runtime
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }
+];
 
 // Implement GraphQL schema
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
   }
 };
 
