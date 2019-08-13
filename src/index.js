5b8de7f795c3e2fa0e5978a7aa479153bfcc5a04
LucSaunders <lucsaunders@gmail.com>
2019-08-13 00:31:35 -0400

    Add Prisma binding instance attached to 'context'.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('./generated/prisma-client');
 
 // Implement GraphQL schema
 const resolvers = {
@@ -22,7 +23,9 @@ const resolvers = {
 // Bundle and pass the schema and resolvers to the GraphQLServer imported from graphql-yoga
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  // Initialize the context object that's passied into all GraphQL resolvers
+  context: { prisma }
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
